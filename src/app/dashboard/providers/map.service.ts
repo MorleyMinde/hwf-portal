7cf2384a053182bfbbb6e145118bb736ce196bc8
Leonard C. Mpande <leo.august27@gmail.com>
2017-06-03 13:51:15 +0300

    merging changes

--- src/app/dashboard/providers/map.service.ts
+++ src/app/dashboard/providers/map.service.ts
@@ -9,53 +9,6 @@ import {Visualization} from "../model/visualization";
 import {Observable} from "rxjs";
 import {MapConfiguration} from "../model/map-configuration";
 import * as _ from 'lodash';
-//
-// module colorModule {
-//   export class Color {
-//     constructor(public red: number, public green: number, public blue: number) {
-//     }
-//
-//     private coll: Array<any> = [this.red, this.green, this.blue];
-//     private valid = this.colorIsVerid(this.coll);
-//     private text = this.colorText(this.coll, 'hex');
-//     private bg = this.colorText(this.coll, 'hex');
-//
-//
-//     colorIsVerid(colorSectionList) {
-//       let isValid = 'n';
-//       if ((!isNaN(colorSectionList[0])) && (!isNaN(colorSectionList[1])) && (!isNaN(colorSectionList[2]))) {
-//         isValid = 'y'
-//       }
-//       return isValid;
-//     }
-//
-//     colorText(colorSectionList: Array<any>, colorFormat: String) {
-//       let base: number = 0;
-//       let denominator: number = 1;
-//       let result: String = '';
-//
-//       if (colorFormat == 'hex') {
-//         base = 16;
-//       }
-//
-//       colorSectionList.forEach((colorSection, colorSectionIndex) => {
-//         let colorSectionSegment = Math.round(colorSection / denominator);
-//         let colorSegmentString = colorSectionSegment.toString(base);
-//
-//         if (colorFormat == 'hex' && colorSegmentString.length < 2) {
-//           colorSegmentString = '0' + colorSegmentString;
-//         }
-//         result = result + colorSegmentString;
-//       })
-//
-//       if (colorFormat == 'hex') {
-//         result = '#' + result.toUpperCase();
-//       }
-//       return result;
-//     }
-//
-//   }
-// }
 
 interface Legend {
   htmlLegend: Object;
@@ -726,7 +679,6 @@ export class MapService {
       let geoFeatureArray: any[] = [];
       visualizationObject.layers.forEach(layer => {
         let geoFeatureParams = layer.settings.hasOwnProperty('geoFeature') ? null : this._getGeoFeatureParameters(layer.settings, visualizationObject.details.filters);
-        console.log(geoFeatureParams)
         geoFeatureArray.push(geoFeatureParams != null ? this._getGeoFeatures(geoFeatureParams) : []);
 
       });
