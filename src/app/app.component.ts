29271c9dc0d1a3c50b2359f561d71f945b66aef1
Vincent P. Minde <vincentminde@gmail.com>
2017-08-19 00:49:15 +0300

    This is fixed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import {CurrentUserService} from "./providers/current-user.service";
+import {Http} from "@angular/http"
+import {HttpClientService} from "./shared/providers/http-client.service";
 
 @Component({
   selector: 'app-root',
@@ -9,9 +11,13 @@ import {CurrentUserService} from "./providers/current-user.service";
 export class AppComponent {
   title = 'Water Point Data Manager';
 
-  constructor(
-    private currentUserService: CurrentUserService
-  ) {
+  dashboards =[]
+  constructor(private currentUserService: CurrentUserService,private http:Http) {
     currentUserService.load();
+    http.get("/api/dashboards.json?fields=id,name").subscribe((dashboards)=>{
+      console.log();
+      this.dashboards = dashboards.json().dashboards;
+    })
+
   }
 }
--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule }     from '@angular/core';
 import {Routes, RouterModule, PreloadAllModules} from '@angular/router';
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/dashboard/home', pathMatch: 'full' },
+  { path: '', redirectTo: '/dashboard/SbnKplppPd0', pathMatch: 'full' },
   {path: 'data-entry', loadChildren: 'app/data-entry/data-entry.module#DataEntryModule'},
   {path: 'data-entry-readonly', loadChildren: 'app/data-entry/data-entry.module#DataEntryModule'},
   {path: 'dashboard/:pageId', loadChildren: 'app/dashboard/dashboard.module#DashboardModule'}
