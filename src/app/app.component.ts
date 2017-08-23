85f59d9a9ca5e40aef8fda2c2cfc08bb3e21ea6f
Vincent P. Minde <vincentminde@gmail.com>
2017-08-23 18:58:32 +0300

    Fixed some issues

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import {CurrentUserService} from "./providers/current-user.service";
 import {Http} from "@angular/http"
 import {HttpClientService} from "./shared/providers/http-client.service";
-import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -15,7 +14,8 @@ export class AppComponent {
   dashboards =[]
   constructor(private currentUserService: CurrentUserService,private http:Http) {
     currentUserService.load();
-    http.get(environment.url + "/api/dashboards.json?fields=id,name").subscribe((dashboards)=>{
+    http.get("/api/dashboards.json?fields=id,name").subscribe((dashboards)=>{
+      console.log();
       this.dashboards = dashboards.json().dashboards;
     })
 
