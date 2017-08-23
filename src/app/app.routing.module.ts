7a5cbc6cbee96d2effcce8a0db1294d76aaff04e
Vincent P. Minde <vincentminde@gmail.com>
2017-08-23 12:55:19 +0300

    Remove data entry module

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -3,8 +3,6 @@ import {Routes, RouterModule, PreloadAllModules} from '@angular/router';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/dashboard/SbnKplppPd0', pathMatch: 'full' },
-  {path: 'data-entry', loadChildren: 'app/data-entry/data-entry.module#DataEntryModule'},
-  {path: 'data-entry-readonly', loadChildren: 'app/data-entry/data-entry.module#DataEntryModule'},
   {path: 'dashboard/:pageId', loadChildren: 'app/dashboard/dashboard.module#DashboardModule'}
 ];
 
