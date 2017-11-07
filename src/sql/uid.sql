c9e257662f48aba903e7f5dc1a55f07e7d758a84
Vincent P. Minde <vincentminde@gmail.com>
2017-11-07 06:49:29 +0300

    Removed sql code

--- src/sql/uid.sql
+++
@@ -1,8 +0,0 @@
-CREATE OR REPLACE FUNCTION uid()
-RETURNS text AS $$
-  SELECT substring('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
-    FROM (random()*51)::int +1 for 1) ||
-    array_to_string(ARRAY(SELECT substring('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
-       FROM (random()*61)::int + 1 FOR 1)
-   FROM generate_series(1,10)), '')
-$$ LANGUAGE sql;
